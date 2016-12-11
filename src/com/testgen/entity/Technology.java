bb182789bc54539fca57256f9f25f76eac626210
brokerji <470304574@qq.com>
2016-12-11 10:48:36 +0800

    增加了Technology类

---
+++ src/com/testgen/entity/Technology.java
@@ -0,0 +1,26 @@
+package com.testgen.entity;
+
+public class Technology {
+    private int techid;
+    private int positionid;
+    private String techname;
+	public int getTechid() {
+		return techid;
+	}
+	public void setTechid(int techid) {
+		this.techid = techid;
+	}
+	public int getPositionid() {
+		return positionid;
+	}
+	public void setPositionid(int positionid) {
+		this.positionid = positionid;
+	}
+	public String getTechname() {
+		return techname;
+	}
+	public void setTechname(String techname) {
+		this.techname = techname;
+	}
+    
+}
